495931c567a06b41b94b5077c000ed534e0b5f56
estevaosantosribeiro <estevaosantosribeiro@gmail.com>
2025-03-12 13:49:08 -0300

    Adicionar arquivos de projeto.

---
+++ JogoDeAdivinhacao/Program.cs
@@ -0,0 +1,94 @@
+﻿    namespace JogoDeAdivinhacao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // loop principal do jogo
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("-------------------------------------");
+                Console.WriteLine("Jogo de Adivinhação");
+                Console.WriteLine("-------------------------------------");
+
+                Console.WriteLine("Escolha um nível de dificuldade:");
+                Console.WriteLine("-------------------------------------");
+                Console.WriteLine("1 - Fácil (10 tentativas)");
+                Console.WriteLine("2 - Normal (5 tentativas)");
+                Console.WriteLine("3 - Difícil (3 tentativas)");
+
+                int totalDeTentativas = 5;
+
+                Console.Write("Digite sua escolha de dificuldade: ");
+                string opcaoDificuldade = Console.ReadLine();
+
+                if (opcaoDificuldade == "1")
+                {
+                    totalDeTentativas = 10;
+                }
+                else if (opcaoDificuldade == "2")
+                {
+                    totalDeTentativas = 5;
+                }
+                else if (opcaoDificuldade == "3") 
+                {
+                    totalDeTentativas = 3;    
+                }
+
+                    // gerar um número secreto aleatório
+                Random geradorNumeros = new Random();
+                int numeroSecreto = geradorNumeros.Next(1, 21);
+
+                // lógica das tentativas do jogo
+                for (int tentativa = 1; tentativa <= totalDeTentativas; tentativa++)
+                {
+                    //Console.Clear();
+                    Console.WriteLine("-------------------------------------");
+                    Console.WriteLine($"Tentativa {tentativa} de {totalDeTentativas}");
+                    Console.WriteLine("-------------------------------------");
+
+                    if (tentativa == totalDeTentativas)
+                    {
+                        Console.WriteLine("-------------------------------------");
+                        Console.WriteLine("Que pena! Você não conseguiu acertar. O número secreto era " + numeroSecreto);
+                        Console.WriteLine("-------------------------------------");
+
+                        break;
+                    }
+
+                    // lógica do jogo
+                    Console.Write("Digite um número: ");
+                    int numeroDigitado = Convert.ToInt32(Console.ReadLine());
+
+                    if (numeroDigitado == numeroSecreto)
+                    {
+                        Console.WriteLine("-------------------------------------");
+                        Console.WriteLine("Você acertou o número secreto!");
+                        Console.WriteLine("-------------------------------------");
+
+                        break;
+                    }
+                    else if (numeroDigitado > numeroSecreto)
+                    {
+                        Console.WriteLine("-------------------------------------");
+                        Console.WriteLine("O número informado é maior que o número secreto!");
+                        Console.WriteLine("-------------------------------------");
+                    }
+                    else if (numeroDigitado < numeroSecreto)
+                    {
+                        Console.WriteLine("-------------------------------------");
+                        Console.WriteLine("O número informado é menor que o número secreto!");
+                        Console.WriteLine("-------------------------------------");
+                    }
+                }
+                
+                Console.Write("Deseja continuar? (S/N): ");
+                string opcaoContinuar = Console.ReadLine().ToUpper();
+
+                if (opcaoContinuar != "S")
+                    break;
+            }
+        }
+    }
+}
