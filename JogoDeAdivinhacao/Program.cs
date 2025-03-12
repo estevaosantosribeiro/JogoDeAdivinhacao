5c529ad21dcc2ff8fcad67c028cc01474ae25ee8
estevaosantosribeiro <estevaosantosribeiro@gmail.com>
2025-03-12 14:20:15 -0300

    Adicionado arquivo README

--- JogoDeAdivinhacao/Program.cs
+++ JogoDeAdivinhacao/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            // loop principal do jogo
+
             while (true)
             {
                 Console.Clear();
@@ -18,47 +18,32 @@
                 Console.WriteLine("2 - Normal (5 tentativas)");
                 Console.WriteLine("3 - Difícil (3 tentativas)");
 
-                int totalDeTentativas = 5;
+                int totalDeTentativas = 0;
 
                 Console.Write("Digite sua escolha de dificuldade: ");
                 string opcaoDificuldade = Console.ReadLine();
 
                 if (opcaoDificuldade == "1")
-                {
                     totalDeTentativas = 10;
-                }
                 else if (opcaoDificuldade == "2")
-                {
                     totalDeTentativas = 5;
-                }
                 else if (opcaoDificuldade == "3") 
-                {
                     totalDeTentativas = 3;    
-                }
 
-                    // gerar um número secreto aleatório
+                // gerar um número secreto aleatório
                 Random geradorNumeros = new Random();
                 int numeroSecreto = geradorNumeros.Next(1, 21);
 
                 // lógica das tentativas do jogo
                 for (int tentativa = 1; tentativa <= totalDeTentativas; tentativa++)
                 {
-                    //Console.Clear();
+                    Console.Clear();
                     Console.WriteLine("-------------------------------------");
                     Console.WriteLine($"Tentativa {tentativa} de {totalDeTentativas}");
                     Console.WriteLine("-------------------------------------");
 
-                    if (tentativa == totalDeTentativas)
-                    {
-                        Console.WriteLine("-------------------------------------");
-                        Console.WriteLine("Que pena! Você não conseguiu acertar. O número secreto era " + numeroSecreto);
-                        Console.WriteLine("-------------------------------------");
-
-                        break;
-                    }
-
                     // lógica do jogo
-                    Console.Write("Digite um número: ");
+                    Console.Write("Digite um número entre 1 e 20: ");
                     int numeroDigitado = Convert.ToInt32(Console.ReadLine());
 
                     if (numeroDigitado == numeroSecreto)
@@ -69,18 +54,30 @@
 
                         break;
                     }
+
+                    if (tentativa == totalDeTentativas)
+                    {
+                        Console.WriteLine("-------------------------------------");
+                        Console.WriteLine("Que pena! Você não conseguiu acertar. O número secreto era " + numeroSecreto);
+                        Console.WriteLine("-------------------------------------");
+
+                        break;
+                    }
                     else if (numeroDigitado > numeroSecreto)
                     {
                         Console.WriteLine("-------------------------------------");
                         Console.WriteLine("O número informado é maior que o número secreto!");
                         Console.WriteLine("-------------------------------------");
                     }
-                    else if (numeroDigitado < numeroSecreto)
+                    else
                     {
                         Console.WriteLine("-------------------------------------");
                         Console.WriteLine("O número informado é menor que o número secreto!");
                         Console.WriteLine("-------------------------------------");
                     }
+
+                    Console.Write("Aperte ENTER para continuar...");
+                    Console.ReadLine();
                 }
                 
                 Console.Write("Deseja continuar? (S/N): ");
--- README.md
+++ README.md
@@ -1,1 +1,49 @@
-# JogoDeAdivinhacao
+# Jogo de Adivinhação
+
+![](https://i.imgur.com/6gsaAJx.gif)
+
+## Sobre o projeto
+
+Este é um jogo simples de adivinhação em C#, onde o jogador tenta adivinhar um número aleatório. A cada tentativa o jogo fornece dicas para ajudar a descobrir o número certo.
+
+## Funcionalidades
+
+- **Número Secreto Aleatório**: No começo do jogo, um número entre 1 e 20 é sorteado para o jogador adivinhar.  
+- **Modos de Dificuldade**: O jogador pode optar entre três dificuldades (Fácil, Médio e Difícil), que determinam a quantidade de tentativas permitidas.  
+- **Respostas em Tempo Real**: A cada palpite, o jogo informa se o número escolhido é maior ou menor que o número secreto.
+
+## Requisitos
+
+- .NET SDK (recomendado .NET 8.0 ou superior) para compilação e execução do projeto.
+
+## Como usar
+
+1. Clone o repositório
+
+```
+git clone https://github.com/estevaosantosribeiro/JogoDeAdivinhacao.git
+```
+
+2. Navegue até a pasta raiz da solução
+
+```
+cd jogoDeAdivinhacao
+```
+
+3. Restaure as dependências
+
+```
+dotnet restore
+```
+
+4. Navegue até a pasta do projeto
+
+```
+cd jogoDeAdivinhacao.ConsoleApp
+```
+
+5. Execute o projeto
+
+```
+dotnet run
+```
